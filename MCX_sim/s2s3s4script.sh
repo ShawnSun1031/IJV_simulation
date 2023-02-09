a2985ab82bfa112f742e0e353901102f827ecd9f
dicky1031 <82211039+dicky1031@users.noreply.github.com>
2023-02-09 13:52:54 +0800

    Delete MCX_sim directory

--- MCX_sim/s2s3s4script.sh
+++
@@ -1,3 +0,0 @@
-python3 S2_make_simfile.py "KB_ijv_small_to_large" 300000000
-python3 S3_run_sim.py "KB_ijv_small_to_large" 1 765
-python3 S4_wmc_generate_dataset.py "KB_dataset_small" "KB_ijv_small_to_large" 1 765
